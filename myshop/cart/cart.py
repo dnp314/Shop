55bcca1e8488e452ff7251cbdc73ce52c3c8d992
Divyesh Parikh <dcparikh360@gmail.com>
2025-02-15 12:10:13 +0530

    Added Cart Templates

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -7,7 +7,7 @@ class Cart:
     self.session = request.session
     cart         = self.session.get(settings.CART_SESSION_ID)
     if not cart:
-      cart = self.session[settings.CART_SESSION_ID]
+      cart = self.session[settings.CART_SESSION_ID]={}
     self.cart=cart
     
   def add(self, product, quantity=1, override_quantity=False):
@@ -29,7 +29,7 @@ class Cart:
     self.session.modified = True
 
   def remove(self, product):
-    product_id = str(product_id)
+    product_id = str(product.id)
     if product_id in self.cart:
       del self.cart[product_id]
       self.save()  
@@ -45,8 +45,7 @@ class Cart:
     for item in cart.values():
       item['price'] = Decimal(item['price'])
       item['total_price'] = item['price']*item['quantity']
-    
-    yield item
+      yield item
   
   def __len__(self):
     return sum(item['quantity'] for item in self.cart.values())   
--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import get_object_or_404,redirect,render
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
-from .forms import CardAddProductForm
-
+from .forms import CartAddProductForm
 # Create your views here.
 
 @require_POST
 def cart_add(request, product_id):
   cart    = Cart(request)
   product = get_object_or_404(Product, id=product_id)
-  form    = CardAddProductForm(request.POST)
+  form    = CartAddProductForm(request.POST)
   
   if form.is_valid():
     cd = form.cleaned_data
@@ -31,5 +30,9 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
   cart = Cart(request)
+  for item in cart:
+    item['update_quantity_form'] = CartAddProductForm(
+      initial={'quantity':item['quantity'],'override':True}
+    )
   return render(request, 'cart/detail.html', {'cart':cart})
   
