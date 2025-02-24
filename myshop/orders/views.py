960228a13bc14bee0ccf67bd2bef0e07d4dd0a17
Divyesh Parikh <dcparikh360@gmail.com>
2025-02-24 19:03:53 +0530

    Added Celery

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,7 +2,7 @@ from cart.cart import Cart
 from django.shortcuts import render
 from .forms import OrderCreateForm
 from .models import OrderItem
-
+from .tasks import order_created
 # Create your views here.
 
 def order_create(request):
@@ -13,16 +13,17 @@ def order_create(request):
     if form.is_valid():
       order = form.save()
   
-    for item in cart:
-      OrderItem.objects.create(
-        order   = order,
-        product = item['product'],
-        price   = item['price'],
-        quantity= item['quantity']
-      )
-    cart.clear()
-  
-    return render(request, 'orders/order/created.html', {'order':order})
+      for item in cart:
+        OrderItem.objects.create(
+          order   = order,
+          product = item['product'],
+          price   = item['price'],
+          quantity= item['quantity']
+        )
+      cart.clear()
+      #launch asynchronous task
+      order_created.delay(order.id)
+      return render(request, 'orders/order/created.html', {'order':order})
 
   else:
     form = OrderCreateForm()
