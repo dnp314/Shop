313a0dd6c0409462875f9a79572638faef1cee69
Divyesh Parikh <dcparikh360@gmail.com>
2025-02-26 18:56:17 +0530

    Added ExportToCSV

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -6,6 +6,39 @@ class OrderItemInLine(admin.TabularInline):
   model = OrderItem
   raw_id_fields= ['product']
   
+import csv 
+import datetime
+from django.http import HttpResponse
+
+def export_to_csv(modeladmin, request, queryset):
+  opts = modeladmin.model._meta
+  content_disposition = (
+    f'attachment; filename={opts.verbose_name}.csv'
+  )  
+  response = HttpResponse(content_type='text/csv')
+  response['Content-Disposition'] = content_disposition
+  writer   = csv.writer(response)
+  fields   = [
+    field
+    for field in opts.get_fields()
+    if not field.many_to_many and not field.one_to_many
+  ]
+  writer.writerow([field.verbose_name for field in fields])
+  
+  for obj in queryset:
+    data_row = []
+    for field in fields:
+      value = getattr(obj, field.name)
+      if isinstance(value, datetime.datetime):
+        value = value.strftime('%d/%m/%Y')
+      data_row.append(value)
+    writer.writerow(data_row)
+  return response
+export_to_csv.short_description = 'Export to CSV'
+  
+  
+  
+  
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
   list_display=[
@@ -20,6 +53,7 @@ class OrderAdmin(admin.ModelAdmin):
     'created',
     'updated'
   ]
+  actions     = [export_to_csv]
   list_filter = ['paid','created','updated']
   inlines     = [OrderItemInLine]
 
